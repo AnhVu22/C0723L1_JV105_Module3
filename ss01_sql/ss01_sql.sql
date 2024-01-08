d0c64cf6c17de319be24f8fec364909e102f6dec
Lê Anh Vũ <135134776+AnhVu22@users.noreply.github.com>
2024-01-08 20:31:33 +0700

    up

--- ss01_sql/ss01_sql.sql
+++ ss01_sql/ss01_sql.sql
@@ -3,19 +3,19 @@ use ss01_sql;
 
 create table if not exists class(
 `id_class` int primary key auto_increment,
-`name_class` varchar(50) unique
+`name_class` varchar(50)
 );
 
 create table if not exists teacher(
 `id_teacher` int primary key auto_increment,
-`name_teacher` varchar(50) unique,
-`age` int unique,
-`country` varchar(50) unique
+`name_teacher` varchar(50),
+`age` int,
+`country` varchar(50)
 );
 
 insert into class (id_class, name_class) value(1,'C0723L1 JV105');
 insert into class value(2,'C0523L1 JV101');
 insert into teacher value(1,'Anh Vũ',31,'VN');
-insert into teacher(id_teacher,name_teacher,age,country) value(2,'Dikei',23,'US');
+insert into teacher(id_teacher,name_teacher,age,country) value(2,'Dikei',23,'Vn');
 select * from class;
 select * from teacher;
