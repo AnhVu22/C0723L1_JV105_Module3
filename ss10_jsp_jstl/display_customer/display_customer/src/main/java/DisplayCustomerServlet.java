ef9d8049d1f17a621c3aabb05412ee640195da7d
Lê Anh Vũ <135134776+AnhVu22@users.noreply.github.com>
2024-02-18 12:01:26 +0700

    update

---
+++ ss10_jsp_jstl/display_customer/display_customer/src/main/java/DisplayCustomerServlet.java
@@ -0,0 +1,8 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value = "/customer")
+public class DisplayCustomerServlet extends HttpServlet {
+
+
+}
